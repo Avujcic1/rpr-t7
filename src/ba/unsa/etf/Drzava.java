e881452242aae5f56af020728814f877139fa873
Avujcic1 <44148907+Avujcic1@users.noreply.github.com>
2018-12-06 20:25:21 +0100

    Priprema

--- src/ba/unsa/etf/Drzava.java
+++ src/ba/unsa/etf/Drzava.java
@@ -2,7 +2,52 @@ package ba.unsa.etf;
 
 public class Drzava {
 
-    public static void main(String[] args) {
-	// write your code here
+    private int brojStanovnika;
+    private String naziv = new String();
+    private double povrsina;
+    private String jedinicaPovrsine = new String();
+    private Grad glavniGrad = new Grad();
+
+    public Drzava() {}
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
     }
+
+    public String getJedinicaPovrsine() {
+        return jedinicaPovrsine;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+
+    public void setJedinicaPovrsine(String jedinicaPovrsine) {
+        this.jedinicaPovrsine = jedinicaPovrsine;
+    }
+
+    public double getPovrsina() {
+        return povrsina;
+    }
+
+    public void setPovrsina(double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
 }
